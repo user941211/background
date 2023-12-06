d205892c1a3be755a5302b550529eb0afbb8b0ad
user941211 <common941211@gmail.com>
2023-12-06 17:14:15 +0900

    0.04버전 업로드

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ import { checkLocationPermissions } from './src/Permissions';
 import sendDataToServer from './src/ServerCommunicator';
 import { startDeviceScan, handleDeviceSelect } from './src/BleManager';
 
+const serviceUUID = '00001679-0000-1000-8000-00805f9b34fb';
+const characteristicUUID = '00002468-0000-1000-8000-00805f9b34fb';
+
 const App = () => {
     const [uuid, setUuid] = useState('');
     const [devices, setDevices] = useState([]);
@@ -15,10 +18,14 @@ const App = () => {
     const manager = new BleManager();
     const timeoutRef = useRef(null);
 
-    
+
     useEffect(() => {
         checkLocationPermissions();
     }, []);
+
+    const isHexadecimal = (str) => {
+        return /^(0x|0X)?[0-9A-Fa-f]{4}$/.test(str);
+    }
 
     const handleButtonClickWithPermissionRequest = () => {
         //uuid는 16진수로 작성되어야 한다.
@@ -37,20 +44,19 @@ const App = () => {
                 "연결 실패", // Alert 제목
                 "연결에 실패했습니다. 다시 찾기 버튼을 누르십시오", // Alert 메시지
                 [
-                    { text: "OK", onPress: () => console.log("OK Pressed") } // 버튼 클릭시 동작
+                    { text: "OK", onPress: () => console.log("버튼 누름!!!") } // 버튼 클릭시 동작
                 ],
                 { cancelable: false } // 사용자가 화면 밖을 터치해도 Alert가 사라지지 않습니다.
             );
-        
+
             // devices 배열에서 첫 번째 장치를 선택
             const device = devices[0];
-        
+
             // 선택한 장치가 존재하면 연결 시도
             if (device) {
                 handleDeviceSelect(manager, device, setDevices, setMacAddress, setMessage, handleButtonClickWithPermissionRequest);
             }
         }, 20000);
-        
     };
 
     useEffect(() => {
@@ -61,10 +67,8 @@ const App = () => {
             const response = await sendDataToServer(url);
             setServerResponse(response);
         };
-        
         fetchData();
     }, []);
-    
 
     useEffect(() => {
         if (selectedDevice) {
@@ -80,7 +84,14 @@ const App = () => {
                 onChangeText={text => setUuid(text)}
                 value={uuid}
             />
-            <Button onPress={handleButtonClickWithPermissionRequest} title="기기 찾기" />
+            {selectedDevice ?
+                <Button onPress={() => setSelectedDevice(null)} title="초기화" /> :
+                <Button onPress={() => { if (!isHexadecimal(selectedDevice)) {
+                        Alert.alert("입력  오류", "16진수 4글자로 구성되어야 합니다.");
+                    } else {handleButtonClickWithPermissionRequest()}
+                }}
+                title="기기 찾기" />
+            }
             <Text>{'16진수 (0~9, 대문자 A부터 F)로 이루어진 4자리 문자열을'}</Text>
             <Text>{'입력하고 기기찾기 버튼을 눌러주세요'}</Text>
             <Text>{message}</Text>
--- src/BleManager.js
+++ src/BleManager.js
@@ -6,6 +6,7 @@ export const startDeviceScan = (manager, setDevices, uuid) => {
     console.log(uuid)
     const fullUuid = `0000${uuid}-0000-1000-8000-00805f9b34fb`;
     
+    //serviceUUID,characteristicUUID는 고정 UUID로 사용 예정, 추후 변경
     manager.startDeviceScan([fullUuid], null, (error, device) => {
         if (error) {
             console.log(error);
@@ -31,11 +32,26 @@ export const handleDeviceSelect = (manager, device, _setDevices, setMacAddress, 
             console.log(`Connected to ${d.name}`);
             setMacAddress(d.id); // Save MAC address of connected device
             setMessage(`${d.name}에 연결되었습니다.`);
-            // 연결 해제 이벤트 리스너 설정
+
             d.onDisconnected((error, disconnectedDevice) => {
                 console.log(`Disconnected from ${disconnectedDevice.name}`);
                 setMessage('연결이 끊어졌습니다. 재연결을 시도합니다.');
                 handleButtonClickWithPermissionRequest(); // 연결 해제 시 재연결 로직 실행
+            });// 연결 해제 이벤트 리스너 설정
+
+            return d.discoverAllServicesAndCharacteristics();
+            
+        })
+        .then((d) => {
+            const serviceUUID = '00001234-0000-1000-8000-00805f9b34fb';
+            const characteristicUUID = '00005678-0000-1000-8000-00805f9b34fb';
+            return manager.readCharacteristicForDevice(device.id, serviceUUID, characteristicUUID);
+        })
+        .then((characteristic) => {
+            const data = characteristic.value; // 아두이노에서 받은 데이터
+            // 이제 이 데이터를 서버로 보낼 수 있습니다
+            return axios.post('https://port-0-pbl-server-57lz2alpkmmh4z.sel4.cloudtype.app/upload', {
+                audioFile: data // Replace with your actual data key and value
             });
         })
         .catch((error) => {
@@ -44,5 +60,4 @@ export const handleDeviceSelect = (manager, device, _setDevices, setMacAddress, 
             handleButtonClickWithPermissionRequest(); // 연결 실패 시 재연결 로직 실행
         });
 };
-
 
